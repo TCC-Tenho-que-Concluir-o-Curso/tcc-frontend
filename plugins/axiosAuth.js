8992581972783581f5ebbee471e4fe0a18db5ddb
Leonardo Veiga <leonardo.porfirio@ccc.ufcg.edu.br>
2023-10-10 15:35:16 -0300

    refactoring header

--- plugins/axiosAuth.js
+++ plugins/axiosAuth.js
@@ -1,8 +1,7 @@
 export default function ({ $axios, store }) {
   // Add a request interceptor to set the common header
   $axios.onRequest((config) => {
-    config.headers['Authorization'] = `${store.state.authUser.uid}`
-
+    config.headers.Authorization = `${store.state.authUser.uid}`
     return config
   })
 }
